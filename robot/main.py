bddd01a8d9852835e8284d8008a77f73889f68e3
putao <513026809@qq.com>
2018-05-24 17:53:42 +0800

    的

--- robot/main.py
+++ robot/main.py
@@ -3,15 +3,21 @@
 
 import asyncio
 import portal
+import logging
 
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(levelname)s - %(message)s')
+# logger = logging.getLogger(__name__)
 
 async def main1(loop):
-    print("main")
+    logging.info("main1")
     p = portal.Portal()
     await p.run()
+    await asyncio.sleep(5)
+    loop.stop()
 
 
 def main():
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main1(loop))
-    # loop.run_forever()
+    asyncio.ensure_future(main1(loop))
+    # loop.run_until_complete(main1(loop))
+    loop.run_forever()
--- robot/portal.py
+++ robot/portal.py
@@ -2,30 +2,39 @@
 # -*- coding: UTF-8 -*-
 
 import asyncio
-
+import tcp_connect
+ 
 
 
 class Portal(object):
 
     def __init__(self):
-        self._Host = "portal-dev.js.xoyo.com"
-        self._Port = 9262
+        self._host = "portal-dev.js.xoyo.com"
+        self._port = 9262
+        self._reader = None
+        self._writer = None
+        self._tcp_connect = tcp_connect.TCPConnect()
 
     async def run(self):
-        await self.connect("www.163.com")
-        # await self.connect(self._Host, self._Port)
+        self._tcp_connect.connect(self._host, self._port)
+        await self._connect()
+        # await self.connect(self._host, self._host)
         pass
 
-    async def connect(self, host, port = 80):
-        reader, writer = await asyncio.open_connection(host, port)
-        header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
-        writer.write(header.encode('utf-8'))
-        await writer.drain()
-        while True:
-            line = await reader.readline()
-            if line == b'\r\n':
-                break
-            print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
-            # break
-        # Ignore the body, close the socket
-        writer.close()
+    async def _connect(self):
+        self._reader, self._writer = await asyncio.open_connection(self._host, self._port)
+        # header = 'GET / HTTP/1.0\r\nHost: %s\r\n\r\n' % host
+        # writer.write(header.encode('utf-8'))
+        # await writer.drain()
+        # while True:
+        #     line = await reader.readline()
+        #     if line == b'\r\n':
+        #         break
+        #     print('%s header > %s' % (host, line.decode('utf-8').rstrip()))
+        #     # break
+        # # Ignore the body, close the socket
+        # writer.close()
+
+    def send(self):
+        self._writer.write()
+        pass
