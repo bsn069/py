358caafd9a1b9c43bbd998c5c5098ab2a5e21858
putao mac13 <513026809@qq.com>
2018-09-12 22:17:06 +0800

    d

--- bsn/swig/shm/src/plug.cpp
+++ bsn/swig/shm/src/plug.cpp
@@ -6,18 +6,46 @@ int add(int ia, int ib) {
     return ia + ib;
 }
 
-int shm_new(int32_t key, uint32_t size) 
-{
-    int iShmId = shmget(key, size, IPC_CREAT | IPC_EXCL);
-    return iShmId;
-}
-
 int test_shm_new()
 {
     return shm_new(12345, 4096);
 }
 
-void* shm_open(int key)
+
+int shm_new(int32_t key, uint32_t size) 
+{
+    int iShmId = shmget(key, size, IPC_CREAT | IPC_EXCL | 0666);
+    return iShmId;
+}
+
+int shm_get_id(int32_t key) 
+{
+    int iShmId = shmget(key, 0, 0666);
+    return iShmId;
+}
+
+void* shm_attach(int key)
 {
     return shmat(key, NULL, 0);
+}
+
+int shm_delete(int iShmId)
+{
+    return shmctl(iShmId, IPC_RMID, NULL);
+}
+
+int shm_detach(void const * pVoid)
+{
+    return shmdt(pVoid);
+}
+
+int shm_size(int iShmId)
+{
+    struct shmid_ds buf;
+    int iFlag = shmctl(iShmId, IPC_STAT, &buf);
+    if( iFlag == -1 )
+    {
+        return -1;
+    }
+    return buf.shm_segsz;
 }
--- bsn/swig/shm/src/plug.h
+++ bsn/swig/shm/src/plug.h
@@ -2,6 +2,12 @@
 int add(int ia, int ib);
 
 int shm_new(int32_t key, uint32_t size);
-void* shm_open(int key);
+int shm_get_id(int32_t key);
+int shm_delete(int iShmId);
+
+int shm_size(int iShmId);
+
+void* shm_attach(int key);
+int shm_detach(void const * pVoid);
 
 int test_shm_new();
